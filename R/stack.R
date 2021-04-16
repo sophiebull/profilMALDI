d1071bf2617571eedfa6f6b27a10c2b781e597c5
Sophie Castel <sophiecastel@trentu.ca>
2021-04-15 22:04:57 -0400

    Working on function to stack

--- R/stack.R
+++ R/stack.R
@@ -20,7 +20,7 @@
 
 # -----------------------------------------------------------------------
 
-stack <- function(dir, standard_pos = "lowerRight", standard_thickness, tolerance = 3){
+stack <- function(dir, standard_pos = "lowerRight", standard_thickness, tolerance = 3, scale.row = 200, scale.col = 200){
   
   standard_positions <- c("upperLeft", "upperRight", "lowerLeft", "lowerRight")
   
@@ -83,57 +83,65 @@ stack <- function(dir, standard_pos = "lowerRight", standard_thickness, toleranc
   }
   
   ###########################
-  # Locate external standard
+  # Add Padding for Rotation
   ###########################
   
-  # initialize list of empty matrices
+  # Just increase padding by a lot (nothing specific)
   
-  standard_list <- vector(mode = "list", length = length(files))
-  standard_list <- lapply(standard_list, FUN = function(x) x <- matrix(rep(0, nrow*ncol), nrow = nrow, ncol = ncol))
+  padded_list <- lapply(data_list, FUN = function(x){
+    scale(x = x$surface, scale.row = scale.row, scale.col = scale.col)
+  })
   
-  # populate with standard heights
+  # determine dimensions
   
-  upper <- standard_thickness + tolerance
-  lower <- standard_thickness - tolerance
+  dim_padded_list <- lapply(padded_list, FUN = function(x){
+    dims <- dim(x)
+    names(dims) <- c("rows","columns")
+    return(dims)
+  })
   
+  
+  ##################################
+  # Align external standard (rotate)
+  ##################################
+  
+  # perform rotation correction, save in a list
+  
+  rotated_list <- lapply(padded_list, FUN = function(x){
+    rotate(x = x, standard_thickness = standard_thickness, tolerance = tolerance, standard_pos = standard_pos)
+    })
+  
+  # determine dimensions
+  
+  dim_rotated_list <- lapply(rotated_list, FUN = function(x){
+    dims <- dim(x)
+    names(dims) <- c("rows","columns")
+    return(dims)
+  })
+  
+  # determine scale factors
+  adj_list <- mapply("/", dim_padded_list, dim_rotated_list, SIMPLIFY = FALSE)
+  
+  adj_list <- lapply(adj_list, FUN = function(x){
+    adjs <- round(x*100)
+    names(adjs) <- c("r.factor", "c.factor")
+    return(adjs)
+  })
+  
+  #########################################
+  # Remove Padding to match original scale
+  #########################################
+  
+  vec.scale.row <- lapply(adj_list, FUN = function(x){x["r.factor"]}) # vector of row scale adjustments
+  vec.scale.col <- lapply(adj_list, FUN = function(x){x["c.factor"]}) # vector of col scale adjustments 
+  
+  reverted_list <- list()
   
   for(i in 1:length(files)){
+    reverted_list[[i]] <- scale(x = rotated_list[[i]], scale.row = vec.scale.row[[i]], scale.col = vec.scale.col[[i]])  
+  }
 
-    logic <- (data_list[[i]]$surface > lower) & (data_list[[i]]$surface < upper)
-    standard_list[[i]][logic] <- data_list[[i]]$surface[logic]
-    
-  }
-  
-  # apply window 
-  
-  nrow <- unlist(unique(dims))[1]
-  ncol <- unlist(unique(dims))[2]
-  
-  ref_nrow <- floor(nrow/2)
-  ref_ncol <- floor(ncol/2)
-  
-  # Window where external standard is approximately located 
-  
-  if(standard_pos == "upperLeft"){
-    standard_window <- list(rows = 1:ref_nrow, cols = 1:ref_ncol)
-  }
-  else if(standard_pos == "upperRight"){
-    standard_window <- list(rows = 1:ref_nrow, cols = ref_ncol:ncol)
-  }
-  else if(standard_pos == "lowerLeft"){
-    standard_window <- list(rows = ref_nrow:nrow, cols = 1:ref_ncol)
-  }
-  else if(standard_pos == "lowerRight"){
-    standard_window <- list(rows = ref_nrow:nrow, cols = ref_ncol:ncol)
-  }
-  
-  # Create list of windows
-  window_list <- list()
-  
-  for(i in 1:length(files)){
-    window_list[[i]] <- standard_list[[1]]$surface[standard_window$rows, standard_window$cols]
-  }
-  
+
   # cut out anything above the standard reference (beyond the zero boundary)
   # start searching from bottom, up and left
   
